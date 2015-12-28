950dceb80c31b9a210801bfb304f331dba0990d8
idryu2 <idryu2@users.noreply.github.com>
2015-12-28 10:08:49 +0900

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,19 +1,9 @@
-//For command line git tutorials
-
-//Local Repository modification
-
-//github modification
-
-
 public class Hello{
 	public static void main(String[] args){		
 	
-		System.out.println("Hello world");
-		System.out.println("Hello git");
-		
-		System.out.println("First");
-
-		System.out.println("2002year btikeeper");
+		System.out.println("hello java")
+		System.out.println("hello js")
+		System.out.println("hello jsp")
 		
 		}
 
